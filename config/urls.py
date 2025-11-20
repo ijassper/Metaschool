4252a40824f52ce05848c3cf26b6c22bd4cf48ca
ijassper <jasspercoach@gmail.com>
2025-11-20 09:53:00 +0000

    메인 페이지를 로그인으로 리디렉션

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    # 최상위 경로('/')로 접속 시 '/accounts/login/'으로 리디렉션
+    path('', RedirectView.as_view(url='/accounts/login/'), name='root'),
+    
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
