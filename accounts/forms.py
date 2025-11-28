b8d0820f8485b780051d745701946ebb92f9a401
ijassper <jasspercoach@gmail.com>
2025-11-28 10:31:05 +0000

    비밀번호 필드 수정

--- accounts/forms.py
+++ accounts/forms.py
@@ -11,7 +11,7 @@ class CustomUserCreationForm(UserCreationForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # [핵심] 모든 필드에 부트스트랩 디자인(form-control) 자동 적용
+        # 모든 필드에 부트스트랩 디자인(form-control) 자동 적용
         for field in self.fields.values():
             field.widget.attrs['class'] = 'form-control'
         
@@ -19,6 +19,18 @@ class CustomUserCreationForm(UserCreationForm):
         self.fields['name'].widget.attrs['placeholder'] = '이름 (예: 홍길동)'
         self.fields['phone'].widget.attrs['placeholder'] = '010-1234-5678'
         self.fields['subject'].widget.attrs['placeholder'] = '담당 과목 (예: 영어, 수학)'
+
+        # 비밀번호 필드에 ID 부여 (자바스크립트가 찾을 수 있게)
+        # (주의: 장고 버전에 따라 필드명이 'password', 'password_confirmation'일 수도 있고 'pass1', 'pass2'일 수도 있음)
+        # 화면에 'Password', 'Password confirmation'이라고 떴으므로 아래 이름을 사용합니다.
+        
+        if 'password' in self.fields:
+            self.fields['password'].widget.attrs['placeholder'] = '비밀번호 (8자 이상)'
+            self.fields['password'].widget.attrs['id'] = 'id_password' # JS용 ID
+            
+        if 'password_confirmation' in self.fields:
+            self.fields['password_confirmation'].widget.attrs['placeholder'] = '비밀번호를 한 번 더 입력하세요'
+            self.fields['password_confirmation'].widget.attrs['id'] = 'id_password_confirm' # JS용 ID
 
     def save(self, commit=True):
         user = super().save(commit=False)
