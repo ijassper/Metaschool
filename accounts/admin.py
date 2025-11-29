532f374a6f8370d1188b254c24fba0c54a713212
ijassper <jasspercoach@gmail.com>
2025-11-29 12:44:47 +0000

    관리자 페이지

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,24 +2,22 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, Student, School, SystemConfig
 
-# 1. 커스텀 유저 관리자 설정
+# 1. 사용자(교사) 관리 화면 설정
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
     list_display = ['email', 'name', 'school', 'subject', 'is_staff']
+    # 관리자 페이지에서 수정할 수 있는 필드 설정
     fieldsets = UserAdmin.fieldsets + (
         (None, {'fields': ('name', 'phone', 'school', 'subject')}),
     )
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        (None, {'fields': ('name', 'phone', 'school', 'subject')}),
-    )
 
-# 2. 시스템 설정 관리자 (편의성 강화)
+# 2. 시스템 설정(API 키) 관리 화면 설정
 class SystemConfigAdmin(admin.ModelAdmin):
     list_display = ['key_name', 'value', 'description', 'updated_at']
     search_fields = ['key_name']
 
-# 3. 모델 등록
+# 3. 관리자 페이지에 등록
 admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Student)
 admin.site.register(School)
-admin.site.register(SystemConfig, SystemConfigAdmin) # ★ 여기 등록됨!
+admin.site.register(SystemConfig, SystemConfigAdmin)    # 여기 등록
