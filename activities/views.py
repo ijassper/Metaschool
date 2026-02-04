53975cb4fe09ac8ee45834297257a6e3f41e9b8b
ijassper <jasspercoach@gmail.com>
2026-02-04 09:38:15 +0000

    import 수정

--- activities/views.py
+++ activities/views.py
@@ -11,7 +11,7 @@ from django.views.decorators.csrf import csrf_exempt
 # 모델과 폼 가져오기
 from .models import Activity, Question, Answer
 from .forms import ActivityForm, QuestionForm, AnswerForm
-from accounts.models import Student, PromptTemplate, PromptCategory, 
+from accounts.models import Student, PromptTemplate, PromptCategory, PromptLengthOption
 
 # 1. 내가 만든 평가 목록 보기
 @login_required
