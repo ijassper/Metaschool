42fa1a2c7a5b99ff449ec507c49b0362d1dd9380
윤창진 <projassper@gmail.com>
2026-05-25 19:50:29 +0900

    디버그 값 변경

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sgx0u5i0exmwpx6r1)9^0=ml!jxh1b4nz#1c)(q(o$5nq4@nj2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'schoolingrid.com', 
